c3072584e5ddea5175dbf699f87fa2b44b0f8d4d
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-12-12 12:06:14 +0530

    Checked condition to not delete a file if its getting processed

--- routes/api/documents.js
+++ routes/api/documents.js
@@ -121,6 +121,8 @@ router.delete("/delete/:id", authenticate, async (req, res) => {
 
     if (!document) throw new HTTPError(400, "File not found");
 
+    if (!document.processing) throw new HTTPError(400, "File is currently getting processed");
+
     deleteFile(req.params.id, (err) => {
       if (err) return res.status(500).json({ success: false, message: "Failed to delete file" });
 
